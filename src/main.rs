c6f9649b3eb373b953c6c94c69356e10c56aa1b6
Kirill Dudnik <prog.me.2017@gmail.com>
2024-09-28 14:19:55 +0200

    runtime hc-sr04 distance measurement

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,24 @@ mod blink;
 mod uuid;
 mod rgb_led;
 mod wifi;
+mod hc_sr04;
 
+use esp_idf_hal::delay::FreeRtos;
+use esp_idf_hal::gpio::PinDriver;
+use esp_idf_hal::timer::{TimerConfig, TimerDriver};
 use esp_idf_svc::hal::peripherals::Peripherals;
 
 fn main() -> anyhow::Result<()> {
     prepare();
-    let _peripherals = Peripherals::take().unwrap();
-    
-    Ok(())
+    let peripherals = Peripherals::take().unwrap();
+    let trigger = hc_sr04::Trigger::from_pin(peripherals.pins.gpio21)?;
+    let echo = hc_sr04::Echo::from_pin(peripherals.pins.gpio19)?;
+    let mut hc_sr04 = hc_sr04::HCSR04::new(trigger, echo);
+
+    loop {
+        log::info!("Distance: {:.2} cm", hc_sr04.measure_avg(10));
+        FreeRtos::delay_ms(100);
+    }
 }
 
 fn prepare() {

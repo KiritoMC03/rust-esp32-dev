b4644d0823b13536a4629639ceb98e5c5859933b
Kirill Dudnik <prog.me.2017@gmail.com>
2024-09-29 23:26:39 +0200

    runtime draw rust.row on ssd1306

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,44 @@ mod hc_sr501;
 mod helpers;
 mod ssd_1306;
 
+use embedded_graphics::image::{Image, ImageRaw};
+use embedded_graphics::{prelude::*, pixelcolor::BinaryColor};
+use esp_idf_hal::delay::{Delay, FreeRtos};
+use esp_idf_hal::gpio::PinDriver;
 use esp_idf_svc::hal::peripherals::Peripherals;
+use ssd1306::{prelude::*, Ssd1306};
 
 fn main() -> anyhow::Result<(), anyhow::Error> {
     prepare();
-    let _peripherals = Peripherals::take()?;    
+    let peripherals = Peripherals::take()?;
+    let mut delay = Delay::new_default();
+    let mut reset = PinDriver::output(peripherals.pins.gpio17)?; // reset
+    let spi = ssd_1306::spi_interface_default(
+        peripherals.spi2,
+        peripherals.pins.gpio18,
+        peripherals.pins.gpio23,
+        peripherals.pins.gpio5,
+        peripherals.pins.gpio16,
+    )?;
+    let mut display = Ssd1306::new(
+        spi,
+        DisplaySize128x64,
+        DisplayRotation::Rotate0,
+    ).into_buffered_graphics_mode();
+    display.reset(&mut reset, &mut delay).expect("Failed to reset display");
+    display.init().expect("Failed to initialize display");
+
+    let raw: ImageRaw<BinaryColor> = ImageRaw::new(include_bytes!("./resources/rust.raw"), 64);
+
+    let im = Image::new(&raw, Point::new(32, 0));
+
+    im.draw(&mut display).unwrap();
+    
+    FreeRtos::delay_ms(50000);
+
+    display.flush().unwrap();
+    log::info!("Display written to");
+    
     Ok(())
 }
 
